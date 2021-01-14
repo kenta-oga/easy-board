e0ebf6c92b6e0b9b85ade28073cca76433c13e7c
kenta-oga <kenta555phi@gmail.com>
2021-01-14 12:27:41 +0900

    createメソッド設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,19 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, , :passcode).merge(user_id: current_user.id)
+  end
 end
