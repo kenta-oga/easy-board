cda5edbb350856d48dfa65849737712245595b91
kenta-oga <kenta555phi@gmail.com>
2021-01-14 18:43:39 +0900

    中間テーブルに情報を保存

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   with_options presence: true do
     validates :name
-    validates :passcode, format: { with: /[0-9]{4}/ }#, length: { is: 4 }
+    validates :passcode, format: { with: /[0-9]{4}/ }
   end
 
   has_many :group_users
