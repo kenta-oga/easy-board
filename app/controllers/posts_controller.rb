67c88b410860f425c83aa08063d2ce187be7a90b
kenta-oga <kenta555phi@gmail.com>
2021-01-19 20:03:44 +0900

    コントローラー設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,30 @@
 class PostsController < ApplicationController
+  before_action :post_find, only: [:show, :edit, :update, :destroy]
   def index
   end
 
   def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    # if @post.save
+    #   redirect_to root_path
+    # else
+    #   render :new
+    # end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :image, :content).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def post_find
+    @post = Post.find(params[:id])
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
   devise_for :users
-  get 'groups/index'
   root to: "groups#index"
   resources :users, only: [:edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update, :destroy] do
