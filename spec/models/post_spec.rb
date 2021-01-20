69bdd2417066a39a810eebb5da0b3f68d34b6e81
kenta-oga <kenta555phi@gmail.com>
2021-01-20 18:56:50 +0900

    テストコード作成途中

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  
+  describe '#create' do
+    before do
+      @post = FactoryBot.build(:post)
+    end
+
+    it '全ての値が存在すれば登録できること' do
+      expect(@post).to be_valid
+    end
+
+    it 'titleが空では登録できないこと' do
+      #binding.pry
+      @post.title = nil
+      @post.valid?
+      expect(@post.errors.full_messages).to include("Title translation missing: ja.activerecord.errors.models.post.attributes.title.blank")
+    end
+  end
 end
