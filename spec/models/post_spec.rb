0583188448cebf28bace0b7ae32d71de9e5e22a1
kenta-oga <kenta555phi@gmail.com>
2021-01-20 23:14:24 +0900

    テストコードのit作成

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,16 +5,35 @@ RSpec.describe Post, type: :model do
     before do
       @post = FactoryBot.build(:post)
     end
+    describe '新規投稿' do
+      context '新規投稿ができる時' do
+        it '全ての値が存在すれば新規投稿ができること' do
+          expect(@post).to be_valid
+        end
+        it 'contentが空でもimageが存在すれば新規投稿ができること'
 
-    it '全ての値が存在すれば登録できること' do
-      expect(@post).to be_valid
-    end
+        end
+        it 'imageが空でもcontentが存在すれば新規投稿ができること'
 
-    it 'titleが空では登録できないこと' do
-      #binding.pry
-      @post.title = nil
-      @post.valid?
-      expect(@post.errors.full_messages).to include("Title can't be blank")
+        end
+      end
+      context '新規投稿ができない時' do
+        it 'titleが空では登録できないこと' do
+          @post.title = nil
+          @post.valid?
+          expect(@post.errors.full_messages).to include("Title can't be blank")
+        end
+        it 'contentとimageが空だと登録できないこと' do
+
+        end
+        it 'userが紐づいていないと新規投稿できない' do
+
+        end
+        it 'groupが紐づいていないと新規投稿できない' do
+        
+        end
+
+      end
     end
   end
 end
