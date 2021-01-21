25f6aba71f85a30ebeef29d5c088d172fc7ab19c
kenta-oga <kenta555phi@gmail.com>
2021-01-21 15:07:14 +0900

    commentのビュー、コントローラー設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,10 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to post_path(@comment.post_id)
+      redirect_to group_post_path(@comment.post.group.id, @comment.post_id)
     else
       @post = @comment.post
+      @group = @post.group
       @comments = @post.comments
       render "posts/show"
     end
