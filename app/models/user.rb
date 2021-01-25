f3da2b71aeed92b379625389f491a3b5db93f23e
kenta-oga <kenta555phi@gmail.com>
2021-01-26 08:59:59 +0900

    ログイン画面日本語に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 
   has_many :group_users
   has_many :groups, through: :group_users
