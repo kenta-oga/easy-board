37434d57eaac55c9f9db4323f9332c7dbb1f2a9a
kenta-oga <kenta555phi@gmail.com>
2021-01-31 20:48:19 +0900

    no method error解除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :post_find, only: [:show, :edit, :update, :destroy]
-  before_action :group_find, only: [:index, :new, :create, :show, :edit]
+  before_action :group_find, only: [:index, :new, :create, :show, :edit, :update]
   before_action :move_root, only: [:edit, :update, :destroy]
   def index
     @posts = @group.posts.includes(:user)
