746e71ff2cc5af49c87006c5ea12d00122100e0b
kenta-oga <kenta555phi@gmail.com>
2021-02-01 01:02:46 +0900

    readテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_031427) do
+ActiveRecord::Schema.define(version: 2021_01_31_155832) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -70,6 +70,16 @@ ActiveRecord::Schema.define(version: 2021_01_21_031427) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "reads", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.boolean "checked"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_reads_on_post_id"
+    t.index ["user_id"], name: "index_reads_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -90,4 +100,6 @@ ActiveRecord::Schema.define(version: 2021_01_21_031427) do
   add_foreign_key "group_users", "users"
   add_foreign_key "posts", "groups"
   add_foreign_key "posts", "users"
+  add_foreign_key "reads", "posts"
+  add_foreign_key "reads", "users"
 end
