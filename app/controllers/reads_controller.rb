8c0bd44b44a37ae5bf59fdea8cf9c726eb025196
kenta-oga <kenta555phi@gmail.com>
2021-02-14 11:55:36 +0900

    read　画面遷移変更

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -1,12 +1,18 @@
 class ReadsController < ApplicationController
   def create
-    @read = current_user.reads.create(post_id: params[:post_id])
-    redirect_back(fallback_location: root_path)
+    if @read = current_user.reads.create(post_id: params[:post_id])
+      redirect_to group_posts_path(group_id: params[:group_id])
+    else
+      render 'posts/show'
+    end
   end
 
   def destroy
-    @read = Read.find_by(post_id: params[:post_id], user_id: current_user.id)
-    @read.destroy
-    redirect_back(fallback_location: root_path)
+    @read = Read.find_by(post_id: params[:post_id], user_id: current_user.id) 
+    if @read.destroy
+      redirect_to group_posts_path(group_id: params[:group_id])
+    else
+      render 'posts/show'
+    end
   end
 end
